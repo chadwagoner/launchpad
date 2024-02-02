6c67d6b71884eb7dfa6f068190cb5c897fc02f9b
Chad <chawagon03@gmail.com>
2024-02-02 17:47:55 -0500

    fix(configure-portainer.sh): fixed permission issue and setting ownership on directory

--- scripts/configure-portainer.sh
+++ scripts/configure-portainer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ### CREATE PORTAINER DIRECTORY
-mkdir -p /opt/portainer/data
+sudo mkdir -p /opt/portainer/data
+
+### SET CORRECT PERMISSIONS
+chown -R ${USER}:${USER} /opt/portainer
 
 ### INSTALL PORTAINER
 docker run -d -p 9443:9443 --name portainer --restart=always -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer/data:/data portainer/portainer-ce:latest
